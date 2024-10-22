8fd65497d8c1b9d42b171beb194ef3e43b753024
Petrando <petrandorichard@gmail.com>
2024-10-22 19:59:16 +0800

    test 3 max profit calculated

--- test3.js
+++ test3.js
@@ -7,25 +7,26 @@ const constraints = { aConst: 5, bConst: 4, total: 100 }
 const countPairFromAMax = () => {
     const { aMin, aMax, bMin, bMax } = constraintsAB
 
-    const pairs = { a: aMax, b: bMax }
-    let a = aMax    
-    let maxPairFound = false
+    const pairs = []
+    let a = aMax
+    let maxTotal = 0    
     while(a >= aMin){
         let b = bMax
         while(b >= bMin){
             const inConstraints = inTotalConstraints(a, b)
             //console.log('a : ', a, ' b : ', b, ' inContstraints : ', inConstraints, ' maxPairFound : ', maxPairFound)
             if(inConstraints){
-                pairs.a = a
-                pairs.b = b
-                maxPairFound = true
-                break;
+                let newTotal = a + b
+                if(newTotal >= maxTotal){
+                    if(newTotal > maxTotal){
+                        pairs.length = 0
+                        maxTotal = newTotal
+                    }                    
+                    pairs.push({a, b})                    
+                }                
             }            
             b--
-        }
-        if(maxPairFound){
-            break;
-        }
+        }        
         a--
     }
 
@@ -35,23 +36,24 @@ const countPairFromAMax = () => {
 const countPairFromBMax = () => {
     const { aMin, aMax, bMin, bMax } = constraintsAB
 
-    const pairs = { a: aMax, b: bMax }
-    let b = bMax    
-    let maxPairFound = false
+    const pairs = []
+    let b = bMax
+    let maxTotal = 0     
     while(b >= bMin){
         let a = aMax
         while(a >= aMin){
             const inConstraints = inTotalConstraints(a, b)            
             if(inConstraints){
-                pairs.a = a
-                pairs.b = b
-                maxPairFound = true
-                break;
+                let newTotal = a + b
+                if(newTotal >= maxTotal){
+                    if(newTotal > maxTotal){
+                        pairs.length = 0
+                        maxTotal = newTotal
+                    }                    
+                    pairs.push({a, b})                    
+                }
             }            
             a--
-        }
-        if(maxPairFound){
-            break;
         }
         b--
     }
@@ -62,13 +64,7 @@ const countPairFromBMax = () => {
 const inTotalConstraints = (a, b) => {
     const { aConst, bConst, total } = constraints
     const totalAB = (a * aConst) + (b * bConst)
-
-    /*
-    console.log('a : ', a)
-    console.log('b : ', b)
-    console.log('totalAB : ', totalAB)
-    console.log('comparison : ', totalAB <= total)
-    */
+    
     if( totalAB <= total ){
         return true
     }   
@@ -90,6 +86,30 @@ const maximizeTeamPerson = () => {
 }
 
 maximizeTeamPerson()
+
+const calculateMaxProfit = ({
+    nettProfit, maxBeforeDiminish, diminishRate
+}) => {   
+    let diminishingProduct = 0
+    let diminishingProfit = nettProfit
+    let totalDiminishingProfit = 0
+    while(diminishingProfit > 0){        
+        diminishingProfit -= diminishRate
+        totalDiminishingProfit += diminishingProfit        
+
+        diminishingProduct += 1
+    }
+
+    console.log('After while diminishingProfit : ', diminishingProfit, ' diminishRate : ', diminishRate)
+    console.log('total diminishingProduct : ', diminishingProduct)
+    return totalDiminishingProfit + (maxBeforeDiminish * nettProfit)
+
+}
+
+console.log('Team A : ', calculateMaxProfit({ nettProfit: 120, maxBeforeDiminish: 40, diminishRate: 2}))
+console.log('Team B : ', calculateMaxProfit({ nettProfit: 60, maxBeforeDiminish: 80, diminishRate: 1}))
+
+
 
 
 
