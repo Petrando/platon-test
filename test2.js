8a2ac39bc7c3d195b2b36e020cff2c95c042584b
Petrando <petrandorichard@gmail.com>
2024-10-23 11:21:07 +0800

    add comment about production limit for test 2

--- test2.js
+++ test2.js
@@ -2,24 +2,31 @@ const calculateMaxProduct = ({
     maxProduction, nettProfit, maxBeforeDiminish, diminishRate
 }) => {
     const diminishingProduct = maxProduction - maxBeforeDiminish
-    const profitableDiminishProduct = (diminishingProduct / diminishRate) - 1    
+    
+    const profitableDiminishProduct = (nettProfit / diminishRate) - 1    
     //here assuming profitableDiminishProduct is integer
+    
     const actualDiminishProduct = profitableDiminishProduct <= diminishingProduct?
         profitableDiminishProduct:diminishingProduct
 
-    return actualDiminishProduct + maxBeforeDiminish
+    return  maxBeforeDiminish + actualDiminishProduct
 
 }
+
+/*
+maxPremiumWidget * premiumWidgetCost = 150 * 80 = 12000
+maxStandardWidget * standardWidgetCost = 200 * 30 = 6000
+total max production cost = 12000 + 6000 = 18000 still lower than limit 20000
+with these specific constraints production limit can be ignored
+*/
 
 const optimumWidgetAmt = () => {
     const premiumWidgetAmt = calculateMaxProduct({ maxProduction: 150, nettProfit: 100, maxBeforeDiminish: 50, diminishRate: 1})
     const standardWidgetAmt = calculateMaxProduct({ maxProduction: 200, nettProfit: 50, maxBeforeDiminish: 100, diminishRate: 0.5})
 
     const optimumAmt = premiumWidgetAmt + standardWidgetAmt
-
-    console.log('premiumWidgetAmt : ', premiumWidgetAmt)
-    console.log('standardWidgetAmt : ', standardWidgetAmt)
+    
     return optimumAmt
 }
 
-console.log(optimumWidgetAmt)
+console.log(optimumWidgetAmt())
