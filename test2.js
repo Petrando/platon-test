348ffcf7326ce2e0f93f5efe3fb79a9b44b93966
Petrando <petrandorichard@gmail.com>
2024-10-23 11:09:56 +0800

    fix: test2.js logic fix

--- test2.js
+++ test2.js
@@ -2,17 +2,24 @@ const calculateMaxProduct = ({
     maxProduction, nettProfit, maxBeforeDiminish, diminishRate
 }) => {
     const diminishingProduct = maxProduction - maxBeforeDiminish
-    console.log('diminishingProduct : ', diminishingProduct)
-    const profitableDiminishProduct = (diminishingProduct / diminishRate) - 1
-    console.log('profitableDiminishProduct : ', profitableDiminishProduct)
+    const profitableDiminishProduct = (diminishingProduct / diminishRate) - 1    
     //here assuming profitableDiminishProduct is integer
     const actualDiminishProduct = profitableDiminishProduct <= diminishingProduct?
         profitableDiminishProduct:diminishingProduct
 
-    console.log('actualDiminishProduct : ', actualDiminishProduct)
     return actualDiminishProduct + maxBeforeDiminish
 
 }
 
-console.log(calculateMaxProduct({ maxProduction: 150, nettProfit: 100, maxBeforeDiminish: 50, diminishRate: 1}))
-console.log(calculateMaxProduct({ maxProduction: 200, nettProfit: 50, maxBeforeDiminish: 100, diminishRate: 0.5}))
+const optimumWidgetAmt = () => {
+    const premiumWidgetAmt = calculateMaxProduct({ maxProduction: 150, nettProfit: 100, maxBeforeDiminish: 50, diminishRate: 1})
+    const standardWidgetAmt = calculateMaxProduct({ maxProduction: 200, nettProfit: 50, maxBeforeDiminish: 100, diminishRate: 0.5})
+
+    const optimumAmt = premiumWidgetAmt + standardWidgetAmt
+
+    console.log('premiumWidgetAmt : ', premiumWidgetAmt)
+    console.log('standardWidgetAmt : ', standardWidgetAmt)
+    return optimumAmt
+}
+
+console.log(optimumWidgetAmt)
